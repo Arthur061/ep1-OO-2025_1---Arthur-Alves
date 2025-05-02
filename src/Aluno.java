0c644281b0714a7c3810a9dd7168826465b574b0
Arthur Alves <arthurgostoso78@gmail.com>
2025-05-01 22:12:08 -0300

    Melhortias na classe aluno. Foi tratado ocaso de matricula repetida e listagem de alunos.

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Aluno extends Menu {
@@ -6,6 +7,7 @@ public class Aluno extends Menu {
     private String curso;
     private Integer opcao; 
     private Integer tipoAluno;
+    public boolean condicao;
 
     public void aluno() {
         Scanner sc = new Scanner(System.in);
@@ -24,35 +26,55 @@ public class Aluno extends Menu {
         }
 
         switch (this.opcao) {
-            case 1 -> {
+            case 1 -> { // CADASTRAR ALUNOS 
                 System.err.println("Vamos seguir com o cadastro");
-                System.err.print("Matricula: ");
-                matricula = sc.nextInt();
-                sc.nextLine();
+                    System.err.print("Matricula: ");
+                    matricula = sc.nextInt();
+                    sc.nextLine();
+                    boolean existe = DadosTXT.verificarSeDadoExiste(matricula);
+                    while (existe) {
+                        System.out.println("O aluno já está cadastrado.");
+                        System.err.print("Matricula: ");
+                        matricula = sc.nextInt();
+                        sc.nextLine();
+                        existe = DadosTXT.verificarSeDadoExiste(matricula); // atualiza o valor da matricula pae
+                    }
                 System.err.print("Nome completo: ");
                 nome = sc.nextLine();
                 System.err.print("Qual curso você está cursando: ");
                 curso = sc.nextLine();
                 System.out.println("");
-                System.out.println("Sua matricula é " + matricula + ", Nome " + nome + " e seu curso é " + curso);
-                AlunosTXT.salvarEmTxt("alunos.txt", String.valueOf(matricula), nome, curso);}
-        }
 
-        System.out.println("");
-        System.out.println("Qual sua condição de aluno? ");
-        System.out.println("1- Sou aluno especial");
-        System.out.println("2- Sou aluno normal");
-        System.out.print("Sua escolha: ");
-        tipoAluno = sc.nextInt();
-        switch (this.tipoAluno) {
-            case 1 -> {
-                System.out.println("Ok, vamos prosseguir com o sistema para alunos especiais!"); 
-                AlunoEspecial especial = new AlunoEspecial();
-                especial.Sistemaesepcial();
+                System.out.println("");
+                System.out.println("Qual sua condição de aluno? ");
+                System.out.println("1- Sou aluno especial");
+                System.out.println("2- Sou aluno normal");
+                System.out.print("Sua escolha: ");
+                tipoAluno = sc.nextInt();
+                if(tipoAluno == 1) {condicao = true;}
+                switch (this.tipoAluno) {
+                    case 1 -> { 
+                        System.out.println("Ok, vamos prosseguir com o sistema para alunos especiais!");
+                        AlunoEspecial especial = new AlunoEspecial();
+                        especial.executarSistema();}
+                    case 2 -> {
+                    System.out.println("Aluno cadastrado!"); 
+                    System.out.println("Voltando para menu aluno");
+                    Aluno Voltando = new Aluno();
+                    Voltando.aluno();}
+                }
+                DadosTXT.salvarEmTxt("alunos.txt", String.valueOf(matricula), nome, curso, condicao);} 
+            case 2 -> { // LISTAR ALUNOS
+                String caminhoArquivo = "alunos.txt";
+                List<String> nomesAlunos = DadosTXT.getNomesAlunos(caminhoArquivo);
+                if (nomesAlunos.isEmpty()) {
+                    System.out.println("Lista atualmente está vazia :(");
+                } else {System.out.println( "-- Lista de todos os alunos cadastrados no sistema --");
+                        for (int i = 0; i < nomesAlunos.size(); i++) {
+                            System.out.println(i + " : "+ nomesAlunos.get(i));}    
+                }
             }
-            case 2 -> {
-                System.out.println("Ok, vamos prosseguir com o sistema para aluno normal");
-            }
-        }
+        } 
+    }   
     }
-}
+
